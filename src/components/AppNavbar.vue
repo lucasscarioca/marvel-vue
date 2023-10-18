12bd7236635073838fc6a526d96bfc41c212f246
Lucas Carioca <lucascarioca97@gmail.com>
2023-10-18 00:02:41 -0300

    chore: setup routes and style variables

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router'
+
+const isOpen = ref(false)
+const toggle = () => {
+  isOpen.value = !isOpen.value
+}
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-primary">
+    <div class="container-fluid py-3 px-3">
+      <RouterLink class="navbar-brand" to="/">
+        <img alt="Marvel logo" src="@/assets/logo.svg" width="139" height="56" class="d-inline-block align-text-top">
+      </RouterLink>
+      <button type="button" class="navbar-toggler border-0"
+        data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation" @click="toggle"
+      >
+        <ion-icon v-if="isOpen" name="close-outline"></ion-icon>
+        <ion-icon v-else name="reorder-three-outline"></ion-icon>
+      </button>
+      <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
+        <hr class="hr"/>
+        <ul class="navbar-nav gap-2">
+          <li class="nav-item">
+            <RouterLink class="nav-link text-white fs-5 fw-bold" aria-current="page" to="/">HOME</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link text-white fs-5 fw-bold" to="/characters">CHARACTERS</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link text-white fs-5 fw-bold" to="/comics">COMICS</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link text-white fs-5 fw-bold" to="/events">EVENTS</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link text-white fs-5 fw-bold" to="/contact">CONTACT</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+ion-icon {
+  font-size: 48px;
+  color: white;
+}
+.navbar-toggler:focus {
+  box-shadow: none;
+}
+.navbar-collapse.show, .navbar-collapse.collapsing {
+  transition: none;
+}
+
+.navbar-collapse.show {
+  height: 100vh;
+  width: 100vw;
+  background-color: var(--bs-primary);
+  top: 100px;
+  left: 0;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Welcome from '../views/Welcome.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +7,27 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Welcome
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/characters',
+      name: 'characters',
+      component: () => import('../views/Characters.vue')
+    },
+    {
+      path: '/comics',
+      name: 'comics',
+      component: () => import('../views/Comics.vue')
+    },
+    {
+      path: '/events',
+      name: 'events',
+      component: () => import('../views/Events.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/Contact.vue')
     }
   ]
 })
